47b451c4113afe8fa98830e9371df775ea5f16a8
Nick Colley <nickcolley7@gmail.com>
2017-04-09 18:01:49 +0100

    Add styles

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Typeahead from '../components/typeahead'
+import Head from 'next/head'
 
 function source (query, syncResults) {
   var results = [
@@ -16,6 +17,9 @@ function source (query, syncResults) {
 
 export default () => (
   <div>
+    <Head>
+      <link rel="stylesheet" href="https://unpkg.com/accessible-typeahead@0.3.5/examples/styled.css" />
+    </Head>
     Welcome to next.js!
     <br/>
     <Typeahead
